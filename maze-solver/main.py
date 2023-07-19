1dfb5c5fd917ba1911edb568162cb3ff12ad956c
Mikaël Sévigny <sevignator@hotmail.com>
2023-07-19 18:51:57 -0400

    Add solution to first Maze Solver exercise

---
+++ maze-solver/main.py
@@ -0,0 +1,32 @@
+from tkinter import Tk, BOTH, Canvas
+
+
+class Window:
+    def __init__(self, width, height):
+        self.__root = Tk()
+        self.__root.title("Maze Solver")
+        self.__canvas = Canvas(self.__root, bg="white", width=width, height=height)
+        self.__canvas.pack(fill=BOTH, expand=1)
+        self.__isRunning = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self):
+        self.__isRunning = True
+
+        while self.__isRunning:
+            self.redraw()
+
+    def close(self):
+        self.__isRunning = False
+
+
+def main():
+    win = Window(800, 600)
+    win.wait_for_close()
+
+
+main()
